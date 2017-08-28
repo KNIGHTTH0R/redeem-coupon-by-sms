6a5a89a0776056191886f8912bb1cfc170fb27f0
Antonis Flangofas <antonisflags@gmail.com>
2017-08-28 08:20:02 +0300

    Connected route with corresponding view file

---
+++ app/Http/Controllers/RedeemBonusController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RedeemBonusController extends Controller
+{
+    public function show()
+    {
+        return view('redeem-bonus');
+    }
+}
